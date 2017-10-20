a3f9047c7f5dbc04aadcc91be6aac5bd5694e162
muhwang12 <32307605+muhwang12@users.noreply.github.com>
2017-10-20 13:42:46 -0400

    Add files via upload

---
+++ Squirrel holes assignment/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <apvector.h>
+
+int countSquirrels(int h[]);
+
+int main() {
+int holes[5] = {};
+
+}
+
+int countSquirrels(int h[], int pres)
+{
+    bool top
+
+    if (h[pres] < h[pres - 1] && h [pres] < h[pres + 1])
+}
