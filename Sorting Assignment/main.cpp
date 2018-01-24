42b316c13a6d436a49061af1bdb43466f8812073
muhwang12 <32307605+muhwang12@users.noreply.github.com>
2018-01-24 13:47:57 -0500

    Finished

--- Sorting Assignment/main.cpp
+++ Sorting Assignment/main.cpp
@@ -1,3 +1,18 @@
+/*****************************************************************************
+ *	Name: Matthew Wang                                                       *
+ *	Course: ICS4U                                                            *
+ *	Date: January 24, 2018                                                   *
+ *	                                                                         *
+ *	Purpose: Use sorting algorithms to sort out numbers                      *
+ *	                                                                         *
+ *	Usage:                                                                   *
+ *	                                                                         *
+ *	Revision History:                                                        *
+ *	                                                                         *
+ *	Known Issues:                                                            *
+ *	                                                                         *
+ *****************************************************************************/
+
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -11,52 +26,49 @@
 #include <allegro5/allegro_native_dialog.h>
 #include <time.h>
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
 //Merge sort from: https://www.geeksforgeeks.org/merge-sort/
 //Quick sort from: https://www.geeksforgeeks.org/quick-sort/
 //Insertion sort from: https://www.geeksforgeeks.org/insertion-sort/
-ALLEGRO_DISPLAY *display;
-ALLEGRO_TIMER *timer;
-ALLEGRO_BITMAP *screenBitmap;
-ALLEGRO_EVENT_QUEUE *event_queue;
+
+ALLEGRO_DISPLAY *display = NULL;
+ALLEGRO_BITMAP *screenBitmap = NULL;
+ALLEGRO_FONT *algorithmFont = NULL;
 
 using namespace std;
-void merge(int arr[], int l, int m, int r);
-void mergeSort(int arr[], int l, int r);
-void printArray(int A[], int size);
+
+//Mergesort functions
+void merge(vector<int> &v, int l, int m, int r);
+void mergeSort(vector<int> &v, int l, int r);
+
+//Quicksort functions
 void swap(int *a, int *b);
-int partition(int arr[], int low, int high);
-void quickSort(int arr[], int low, int high);
-void insertionSort(int arr[], int n);
-void drawList(int data[], int data_size, int pos);
-void randomNum(int arr[]);
-char chooseOption();
+int partition(vector<int> &v, int low, int high);
+void quickSort(vector<int> &v, int low, int high);
+
+//Insertion sort function
+void insertionSort(vector<int> &v, int n);
+
+void randomNum(vector<int> &vector);
+void printVector(vector<int> &vector);
+
 char chooseSort();
-void drawList(int arr[], int size, int pos);
-bool allegroEventLoop();
-bool initAllegro(const char* title, const int windowWidth_c,
-		const int windowHeight_c);
+void selectSort(char choice, vector<int> &v);
+
+void drawList(vector<int> &v, int pos);
+
+bool initAllegro(const char* title);
 void cleanUp();
-int windowWidth;
-int windowHeight;
 void clearBitmap();
 
-struct Person {
+int windowWidth_c = 640;
+int windowHeight_c = 480;
+int numSwap = 0;
+char c;
 
-    apstring Name;
-    apstring Citizenship;
-    apstring Rank;
-    apstring Worth;
-    int age;
-
-};
-
-struct Billionaire {
-
-    Person person;
-    apstring companyName;
-};
-
+//Timer for tracking how long it takes for a sorting algorithm to run
 class Timer
 {
 public:
@@ -101,149 +113,122 @@ private:
 
 int main()
 {
-
     Timer timer;
 
     srand(NULL);
-    initAllegro("Sort", 640, 480);
-    int arr[200];
-    //vector<Billionaire> bList;
-    int arr_size = sizeof(arr)/ sizeof(arr[0]);
-
-    randomNum(arr);
+    initAllegro("Sort");
+    vector<int> vec (200);
+    randomNum(vec);
 
     cout << "Given: " << endl;
-    printArray(arr, arr_size);
+    printVector(vec);
 
+    c = chooseSort();
     timer.start();
-    mergeSort(arr, 0, arr_size-1);
+    selectSort(c, vec);
     timer.stop();
 
+    cout << endl;
     cout << "Sorted: " << endl;
-    printArray(arr, arr_size);
+    printVector(vec);
 
+    cout << endl;
+    cout << "Number of swaps: " << numSwap << endl;
     std::cout << "Seconds: " << timer.elapsedSeconds() << std::endl;
     std::cout << "Milliseconds: " << timer.elapsedMilliseconds() << std::endl;
 
-
+    al_rest(5);
+    cleanUp();
+    return 0;
 }
 
-/*char chooseOption()
-{
-    char choice;
-    cout << "(L)oad Data" << endl;
-    cout << "(S)ave Data" << endl;
-    cout << " Sort (D)ata " << endl;
-
-    while(choice != 'L' || choice != 'S' || choice != 'D')
-    {
-        cout << "Choose what you want to do: ";
-        cin >> choice;
-        cout << endl;
-    }
-
-    return choice;
-}
-
-void selectCommand(char choice)
-{
-    switch (choice){
-
-    case 'L': readFile();
-              break;
-
-    case 'S': break;
-
-    case 'D': break;
-
-    default: break;
-    }
-}*/
-
+//Function to choose sorting algorithm
 char chooseSort()
 {
     char choice;
-    cout << "(I)nsertion Sort" << endl;
-    cout << "(Q)uick Sort" << endl;
-    cout << "(M)erge Sort" << endl;
+    cout << endl << endl
+         << "(I)nsertion Sort" << endl
+         << "(Q)uick Sort" << endl
+         << "(M)erge Sort" << endl;
 
-    while(choice != 'I' || choice != 'Q' || choice != 'M')
+    while(choice != 'I' && choice != 'Q' && choice != 'M')
     {
         cout << "Choose a sorting algorithm: ";
         cin >> choice;
-        cout << endl;
     }
+
     return choice;
 }
 
-/*void loadFile(vector<Billionaire> list)
+//Takes choice and directs it towards its associated algorithm
+void selectSort(char choice, vector<int> &v)
 {
-    apstring fileName;
-    ifstream fileIn;
-    cout << "Enter file name (include .csv): " << endl;
-    cin >> fileName;
-    fileIn.open(fileName);
+
+    switch (choice){
+
+        case 'I':   insertionSort(v, v.size());
+                    break;
+
+        case 'M':   mergeSort(v, 0, v.size()-1);
+                    break;
+
+        case 'Q':   quickSort(v, 0, v.size()-1);
+                    break;
+
+    }
 
 
-}*/
+}
 
-/*void printBillionaire(Billionaire p)
-{
-    cout << "Name of Billionaire: " << p.person.Name << endl;
-         << "Rank: " << p.person.Rank << endl
-         << "Worth: " << p.person.Worth << " billion" << endl
-         << "Company Name: " << p.companyName << endl
-         << "Age: " << p.person.age << endl
-         << "Citizenship: "<< p.person.Citizenship << endl;
-
-}*/
-
-void randomNum(int arr[])
+//Creates a vector of random integers
+void randomNum(vector<int> &vector)
 {
     int count = 0;
     for(int i = 0; i < 200; i++){
         count++;
-        arr[i] = rand() % 200 + 1;
+        vector[i] = rand() % 200 + 1;
     }
     cout << "Count: " << count << endl;
 }
 
-void merge(int arr[], int l, int m, int r)
+//Merges vectors together
+void merge(vector<int> &v, int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 =  r - m;
 
-    /* create temp arrays */
-    int L[n1], R[n2];
-
-    /* Copy data to temp arrays L[] and R[] */
+    /* create temp vectors */
+    vector<int> L(n1);
+    vector<int> R(n2);
+    /* Copy data to temp vectors L[] and R[] */
     for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
+        L[i] = v[l + i];
     for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
+        R[j] = v[m + 1 + j];
 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    /* Merge the temp vectors back into v[l..r]*/
+    i = 0; // Initial index of first subvector
+    j = 0; // Initial index of second subvector
+    k = l; // Initial index of merged subvector
     while (i < n1 && j < n2){
-        drawList(arr, 200, k);
         if (L[i] <= R[j]){
-            arr[k] = L[i];
+            v[k] = L[i];
             i++;
+            numSwap++;
         }
         else{
-            arr[k] = R[j];
+            v[k] = R[j];
             j++;
         }
         k++;
+        drawList(v, k);
     }
 
     /* Copy the remaining elements of L[], if there
        are any */
     while (i < n1){
-        arr[k] = L[i];
+        v[k] = L[i];
         i++;
         k++;
     }
@@ -251,15 +236,15 @@ void merge(int arr[], int l, int m, int r)
     /* Copy the remaining elements of R[], if there
        are any */
     while (j < n2){
-        arr[k] = R[j];
+        v[k] = R[j];
         j++;
         k++;
     }
 }
 
 /* l is for left index and r is right index of the
-   sub-array of arr to be sorted */
-void mergeSort(int arr[], int l, int r)
+   sub-vector of v to be sorted */
+void mergeSort(vector<int> &v, int l, int r)
 {
     if (l < r){
         // Same as (l+r)/2, but avoids overflow for
@@ -267,10 +252,9 @@ void mergeSort(int arr[], int l, int r)
         int m = l+(r-l)/2;
 
         // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m+1, r);
-
-        merge(arr, l, m, r);
+        mergeSort(v, l, m);
+        mergeSort(v, m+1, r);
+        merge(v, l, m, r);
     }
 }
 //Swaps two elements
@@ -279,97 +263,98 @@ void swap(int& a, int& b)
     int n = a;
     a = b;
     b = n;
+    numSwap++;
 }
 
 /* This function takes last element as pivot, places
    the pivot element at its correct position in sorted
-    array, and places all smaller (smaller than pivot)
+    vector, and places all smaller (smaller than pivot)
    to left of pivot and all greater elements to right
    of pivot */
-int partition (int arr[], int low, int high)
+int partition (vector<int> &v, int low, int high)
 {
-    int pivot = arr[high];    // pivot
+    int pivot = v[high];    // pivot
     int wall = (low - 1);  // Index of smaller element
 
     for (int j = low; j <= high- 1; j++){
         // If current element is smaller than or
         // equal to pivot
-        if (arr[j] <= pivot){
+        if (v[j] <= pivot){
             wall++;    // increment index of smaller element
-            swap(arr[wall], arr[j]);
+            swap(v[wall], v[j]);
+            drawList(v, j);
         }
     }
-    swap(arr[wall + 1], arr[high]);
+    swap(v[wall + 1], v[high]);
     return (wall + 1);
 }
 
 /* The main function that implements QuickSort
- arr[] --> Array to be sorted,
+  v --> vector to be sorted,
   low  --> Starting index,
   high  --> Ending index */
-void quickSort(int arr[], int low, int high)
+void quickSort(vector<int> &v, int low, int high)
 {
     if (low < high)
     {
-        /* pi is partitioning index, arr[p] is now
+        /* pi is partitioning index, v[p] is now
            at right place */
-        int pi = partition(arr, low, high);
+        int pi = partition(v, low, high);
 
         // Separately sort elements before
         // partition and after partition
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        quickSort(v, low, pi - 1);
+        quickSort(v, pi + 1, high);
     }
 }
 
-    /* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n)
+//Function to sort a vector using insertion sort
+void insertionSort(vector<int> &v, int n)
 {
    int key, j;
    for (int i = 1; i < n; i++){
-       key = arr[i];
+       key = v[i];
        j = i-1;
 
-       /* Move elements of arr[0..i-1], that are
+       /* Move elements of v[0..i-1] that are
           greater than key, to one position ahead
           of their current position */
-       while (j >= 0 && arr[j] > key){
-           arr[j+1] = arr[j];
+       while (j >= 0 && v[j] > key){
+           v[j+1] = v[j];
            j = j-1;
+           drawList(v, j);
+           numSwap++;
        }
-       arr[j+1] = key;
+       v[j+1] = key;
+
     }
 
 }
 
-void printArray(int A[], int size)
+//Prints out numbers in vector
+void printVector(vector<int> &vector)
 {
-    int i;
-    for (i = 0; i < size; i++)
-        cout << A[i] << " " << endl;
+
+    for (int i = 0; i < vector.size(); i++){
+        if(i == vector.size()-1)
+        cout << vector[i];
+        else
+        cout << vector[i] << ", ";
+    }
+
 }
 
-bool initAllegro(const char* title, const int windowWidth_c,
-		const int windowHeight_c) {
+//Initializes allegro routines
+bool initAllegro(const char* title) {
 
 	//initialize display
 	al_init();
 	display = al_create_display(windowWidth_c, windowHeight_c);
-	windowWidth = windowWidth_c;
-	windowHeight = windowHeight_c;
 
 	// Always check if your allegro routines worked successfully.
 	if (!display) {
 		std::cerr << "allegro error: failed to initialize display!"
 				<< std::endl;
-		return false;
-	}
-    int fps = 60;
-	ALLEGRO_TIMER *timer = al_create_timer(1.0 / fps);
-
-	if (!timer) {
-		std::cerr << "allegro error: failed to create timer!" << std::endl;
-		al_destroy_display(display);
 		return false;
 	}
 
@@ -378,40 +363,27 @@ bool initAllegro(const char* title, const int windowWidth_c,
 	if (!screenBitmap) {
 		std::cerr << "allegro error: failed to create bitmap!" << std::endl;
 		al_destroy_display(display);
-		al_destroy_timer(timer);
 		return false;
 	}
 
-	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
-	if (!event_queue) {
-		std::cerr << "allegro error: failed to create allegro event queue!"
-				<< std::endl;
-		al_destroy_bitmap(screenBitmap);
-		al_destroy_display(display);
-		al_destroy_timer(timer);
-		return false;
-	}
-
-	if(!al_install_mouse()) {
-		std::cerr << "allegro error:  could not init mouse'" << std::endl;
-		cleanUp();
-		return false;
-	}
 
 	if (!al_init_primitives_addon()) {
 		std::cerr << "allegro error: could not init primitives" << std::endl;
-		al_destroy_event_queue(event_queue);
+		//al_destroy_event_queue(event_queue);
 		al_destroy_bitmap(screenBitmap);
 		al_destroy_display(display);
-		al_destroy_timer(timer);
+		//al_destroy_timer(timer);
 		return false;
 	}
 
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-	al_register_event_source(event_queue, al_get_mouse_event_source());
+	al_init_font_addon(); // initialize the font addon
+    al_init_ttf_addon(); // initialize the ttf (True Type Font) addon
 
-	al_start_timer(timer);
+    algorithmFont = al_load_ttf_font("font.ttf", 30, 0);
+	if (!algorithmFont) {
+		cerr << "fatal error: could not load 'font.ttf'" << endl;
+		return false;
+	}
 
 	al_clear_to_color(al_map_rgb(0, 0, 0));
 	al_set_window_title(display, title);
@@ -420,57 +392,58 @@ bool initAllegro(const char* title, const int windowWidth_c,
 	return true;
 }
 
-bool allegroEventLoop() {
-
-	while (true) {
-		ALLEGRO_EVENT ev;
-		al_wait_for_event(event_queue, &ev);
-
-		if (ev.type == ALLEGRO_EVENT_TIMER) {
-
-		} else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-
-			return false;
-		}
-		if (ev.type == ALLEGRO_EVENT_MOUSE_AXES
-				|| ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY) {
-
-
-		} else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-
-		}
-
-	}
-	return true;
-}
+//Clears bitmap
 void clearBitmap()  {
-		//this should clear the bitmap
 		al_clear_to_color(al_map_rgb(0,0,0));
 	}
-void drawList(int arr[], int size, int pos) {
-	static float xratio = ((float)windowWidth) / size;
-	static float yratio = ((float)windowHeight) / size;
 
-	static ALLEGRO_COLOR green = al_map_rgb(0, 0xff, 0);
-	static ALLEGRO_COLOR red = al_map_rgb(0xff, 0, 0);
+//Draws list onto screen
+void drawList(vector<int> &v, int pos) {
+	static float xratio = ((float)windowWidth_c) / v.size();
+	static float yratio = ((float)windowHeight_c) / v.size();
+
+	static ALLEGRO_COLOR green = al_map_rgb(0, 255, 0);
 
 	clearBitmap();
-	for (int r = 0; r < size; r++) {
+	for (int r = 0; r < v.size(); r++) {
+	    float ratio = v[r] / (float)200;
+
+	    switch(c){
+
+            case 'I': al_draw_textf(algorithmFont, al_map_rgb(255, 255, 255), 125, 50,
+                              ALLEGRO_ALIGN_CENTER, "Insertion Sort");
+                      break;
+
+            case 'Q': al_draw_textf(algorithmFont, al_map_rgb(255, 255, 255), 125, 50,
+                              ALLEGRO_ALIGN_CENTER, "Quick Sort");
+                      break;
+
+            case 'M': al_draw_textf(algorithmFont, al_map_rgb(255, 255, 255), 125, 50,
+                              ALLEGRO_ALIGN_CENTER, "Merge Sort");
+                      break;
+
+
+	    }
+
 		if (r == pos) {
-            al_draw_rounded_rectangle(r * xratio, windowHeight - arr[r]* yratio, (r+1)* xratio, windowHeight , 0.25*xratio, 0.25*xratio, red,(4<xratio/2)?4:(int)xratio/2);
-		} else {
-            al_draw_rounded_rectangle(r * xratio, windowHeight - arr[r]* yratio, (r+1)* xratio, windowHeight , 0.25*xratio, 0.25*xratio, green,(4<xratio/2)?4:(int)xratio/2);
+			al_draw_filled_rounded_rectangle(r * xratio, windowHeight_c - v[r]* yratio, (r+1)* xratio, windowHeight_c, 0.25*xratio, 0.25*xratio, green);
+			//if(xratio > 2) al_draw_rounded_rectangle(r * xratio, windowHeight_c - v[r]* yratio, (r+1)* xratio, windowHeight_c, 0.25*xratio, 0.25*xratio, green,(4<xratio/2)?4:(int)xratio/2);
+		}
+        else {
+			al_draw_filled_rounded_rectangle(r * xratio, windowHeight_c - v[r]* yratio , (r+1)* xratio, windowHeight_c, 0.25*xratio, 0.25*xratio, al_map_rgb(160, 255*ratio/2, 255*ratio));
+			//if(xratio > 2) al_draw_rounded_rectangle(r * xratio, windowHeight_c - v[r]* yratio, (r+1)* xratio, windowHeight_c, 0.25*xratio, 0.25*xratio, al_map_rgb(255, 255*ratio/2, 255*ratio),(4<xratio/2)?4:(int)xratio/2);
 		}
 	}
 
 	al_flip_display();
+
 }
 
 void cleanUp() {
 	al_destroy_bitmap(screenBitmap);
 	al_destroy_display(display);
-	al_destroy_timer(timer);
-	al_destroy_event_queue(event_queue);
+	al_destroy_font(algorithmFont);
 }
+
 
 
